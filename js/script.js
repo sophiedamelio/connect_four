dc21128e26e69b0f76862e4489f8e408e05fa9e0
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2021-12-10 15:58:49 -0800

    Change the makeup of HTML from table to div elements for the cells, begin editing the JavaScript to correspond with this change.

--- js/script.js
+++ js/script.js
@@ -13,29 +13,24 @@ let whoseTurn;
 let winner;
 
 // parts of the DOM we will be constantly updating - like nameEls
-const tableEl = document.querySelector('table');
-
-const tableDataEls = document.querySelectorAll('td'); // value?
-const tableRowEls = document.querySelectorAll('tr');
+const gridEl = document.querySelector('.grid');
+const cellEls = gridEl.querySelectorAll('div');
 
 const playerOneNameEl = document.getElementById('player-one-name'); // value?
 const playerTwoNameEl = document.getElementById('player-two-name'); // value?
 
 // replacing the value of a single cell test
-tableEl.rows[0].cells[2].value = `hello`;
+// tableEl.rows[0].cells[2].value = `hello`;
 
 // this retrives the value of row 1 columm 1
 // let cell = tableEl.rows[0].cells[0];
 // console.log(cell);
 
 // set value of all cells to null upon page loading
-tableRowEls.forEach(tableRowEl => {
-    tableRowEl.value = null;
-})
-
-tableDataEls.forEach(tableRowEl => {
-    tableRowEl.value = null;
-})
+let cellEl;
+for (let i = 0; i < cellEls.length; i++) {
+    cellEl.value = null;
+};
 
 
 document.querySelector('#player-one-button').addEventListener('click', (e) => {
