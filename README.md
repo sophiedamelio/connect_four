d28653a8b3eecea1a5a64ba541654316edf98d70
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2021-12-16 17:37:33 -0800

    Finish README.md, combine reload into init() function, clean up notes and commented out code.

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 ## Connect Four Browser Game
+
 ---
+
 The game is connect four, and the objective is to successfully place four chips in a row (either diagonally, across, up, or down).
+
+Rooted in Tic Tac Toe logic, Connect Four has over four trillion gameboard positions. It is has been a well loved game dating back to 1974, while speculated to have been played many years earlier. There are actually Connect Four tournaments held annually across the globe [like this one](https://www.luvimmigration.com/connect-4.html).
 
 ---
 
 ### Opening Screen, Turns are Displayed at the Top of the Gameboard
-
 ![Connect Four Screenshot of Gameboard Before Playing](https://i.imgur.com/ZginIfJ.png)
 
-### The Winner is Indicated at the Top of the Gameboard
+----
 
+### The Winner is Indicated at the Top of the Gameboard
 ![Connect Four Screenshot of Gameboard After a Player Wins](https://i.imgur.com/uqSJjVN.png)
 
 ---
@@ -18,18 +22,28 @@ The game is connect four, and the objective is to successfully place four chips 
 - JavaScript
 - HTML
 - CSS
+
 ----
 
 ### Getting Started:
-To play the game, simply click anywhere on the slot you wish to place your chip, and your chip will fall into the lowest available slot. Each chip placed will stack on top of the previous, and so on.
+To play the game, simply click anywhere on the slot you wish to place your chip, and your chip will fall into the lowest available slot. Each chip placed will stack on top of the previous, and so on. If the entire gameboard is filled with chips and there are no four in a row's of any one color, it is a tie.
 
 ---
 
-### Link to play the game: [Connect Four](link)
+### Link to play the game: [Connect Four](https://sophiedamelio.github.io/connect_four)
 
 ---
 
-### Next Steps:
+### Planned future enhancments:
+- Make input areas for the player's names.
+
+- Make the winner announcement and replay button pop up over the gameboard.
+
+- Make an alert pop up if you try to place a chip in a slot that is entirely filled up.
+
+- Make the click event listener right above the gameboard, so that you can only click at the top of each slot. Additionally add animation so that it looks like the chips are actually falling to the lowest available spot.
+
+---
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+// constant variables, will not change throughout the game
 const players = {
     1: {
         name: null,
@@ -12,134 +13,112 @@ const players = {
     }
 };
 
-// state variables aka MODEL / source of truth for the app
+// state variables
 let whoseTurn;
 let winner;
+let count;
 
-
-// cached Element references from the DOM
+// cached element references from the DOM
 let gridEl = document.querySelector('.grid');
 let cellEls = document.querySelectorAll('.cell');
 let winnerAnnouncementEl = document.querySelector('#winner-announcement');
 let whoseTurnMessage = document.querySelector('.whose-turn');
 let replayButtonEl = document.querySelector('#replay');
 
-// this makes the two dimensional array that is the grid
+// this creates the two dimensional array that is the grid
 gridEl = new Array(7).fill(0).map(() => new Array(6).fill(0));
 
-
-// this calls the init() function when the replayButtonEl is clicked, resetting the gameboard values
+// this event listeneter is attached to the replay button, and calls the init() function when clicked, resetting the game to be played again
 replayButtonEl.addEventListener('click', e => {
     init()
 });
 
-// this is the event listener on the entire gridEl
-// document.querySelector('.grid').addEventListener('click', handleGridClick, true)
-
-// this function is being called when the page loads
+// this envokes the init() function immediately upon the page loading
 init();
 
-// initializes state variables
-// updates the model with the initial values of the game (update the state with the initial values)
+// this function initializes state variables, updating the model with the initial values of the game
 function init() {
     whoseTurn = Math.floor(Math.random() * 2) + 1;
     winner = null;
-
+    count = 0;
+    winnerAnnouncementEl.textContent = '';
+    document.querySelector('.grid').addEventListener('click', handleGridClick, true)
+    // this sets the background color of each cellEl to white
+    cellEls.forEach(cellEls => {
+        cellEls.style.backgroundColor = 'white';
+    })
     render();
-    reload();
 }
 
-// visualize the state
+// this function visualize the state variables
 function render() {
     whoseTurnMessage.textContent = `It is player ${whoseTurn}'s turn`;
-
 }
 
-// controller function - update the state when interaction happpens - when a user clicks on the grid, update our state
+// this is the controller function - it updates the state when the user click interaction happpens on the grid
 function handleGridClick(e) {
     if (e.target.className === 'cell') {
-
-        // e.target.parentElement = the slot that was clicked on
+        // e.target.parentElement is the slot that was clicked on
         let children = e.target.parentElement.children
-        console.log(children)
-
-        // this accesses the clicked on slot's children at an index of 0 (tallest cell)
-        // console.log(children[0].innerHTML);
-        
+        // this for loop goes through each cell of the slot that was clicked on, and changes the color of the lowest available cell to the color of the player whose turn it is
         for (let i = children.length - 1; i >= 0; i--) {
             if (children[i].style.backgroundColor === 'white') {
-                // children[i].innerHTML = whoseTurn.toString();
                 children[i].style.backgroundColor = players[whoseTurn].color;
                 break;
             }
         }
     };
 
-    // this makes it so that whoseTurn switches off each time a click happens
+    // this updates the value of the variable count by adding 1 every time a click is registered, and renders a tie message when the entire board has been filled and no winner announcement has been made.
+    count++;
+    if (count >= 42 && winnerAnnouncementEl.textContent === '') {
+        winnerAnnouncementEl.textContent = `It is a tie! No one wins.`
+    }
+
+    // this updates the whoseTurn variable so that it switches off between the players each time a click happens
     if (whoseTurn === 1) {
         whoseTurn = 2;
     } else {
         whoseTurn = 1;
     }
 
+    // this function is called to update the view each time a click is registered
     render();
+    // this function is called to check the winner each time a click is registered
     checkWinner();
 };
 
-// this is a helper function, that sets the innerhtml of each page to 0 and clears the winner announcement.
-// this function is called within init()
-function reload () {
-    cellEls.forEach(cellEls => {
-        cellEls.style.backgroundColor = 'white';
-    })
-    // this sets the winnerAnnouncementEl text content to be '' initially
-    winnerAnnouncementEl.textContent = '';
-    // adds an event listener every time init() is called
-    document.querySelector('.grid').addEventListener('click', handleGridClick, true)
-};
-
-
-
+// this function checks for a winner, and is called above, each time a click occurs
 function checkWinner () {
-    // this is checking each array in winningArrays
-    // if winningArrays at each index i's [0], 1, 2, 3 correspond with each squareOne, two, three, 
-    //then there is a winner
+    // this for loop is checking each array in winningArrays
     for (let i = 0; i < winningArrays.length; i++) {
+        // if winningArrays at each index i's [0], 1, 2, 3 correspond with each squareOne, two, three, 
+    //then there is a winner
+        // this declares squareOne, SquareTwo, squareThree, and squareFour, which represent the four chips to be tested for a win
+        // th
         const squareOne = cellEls[winningArrays[i][0]]
         const squareTwo = cellEls[winningArrays[i][1]]
         const squareThree = cellEls[winningArrays[i][2]]
         const squareFour = cellEls[winningArrays[i][3]]
 
-        // console.log(squareOne, squareTwo, squareThree, squareFour)
-        // console.log(squareOne.style.backgroundColor)
-    // nested for loop to ? ^ this prints ALL the possible backgrounds, mostly white, and these if statments 
-    // are not working
         if (squareOne.style.backgroundColor === 'rgb(229, 184, 182)' &&
             squareTwo.style.backgroundColor === 'rgb(229, 184, 182)' &&
             squareThree.style.backgroundColor === 'rgb(229, 184, 182)' &&
             squareFour.style.backgroundColor === 'rgb(229, 184, 182)') {
                 winnerAnnouncementEl.textContent = `Player 1 is the winner!`
-                // removes event listener when a winner is declared
+                // this removes event listener when a winner is declared, so that you cannot keep playing
                 document.querySelector('.grid').removeEventListener('click', handleGridClick, true)
-
             }
         if (squareOne.style.backgroundColor === 'rgb(216, 123, 87)' &&
             squareTwo.style.backgroundColor === 'rgb(216, 123, 87)' &&
             squareThree.style.backgroundColor === 'rgb(216, 123, 87)' &&
             squareFour.style.backgroundColor === 'rgb(216, 123, 87)') {
                 winnerAnnouncementEl.textContent = `Player 2 is the winner!`
+                // this removes event listener when a winner is declared, so that you cannot keep playing
                 document.querySelector('.grid').removeEventListener('click', handleGridClick, true)
-                console.log('the event listener is removed?')
         }
     }
 }
-
-// their own loop through children?? if (all cellEls !== white) {
-//     winner annuouncement tie
-// }
-// tie function 
-// if all cellEls.style.background (children?) do not equal white
-// winnerAccouncementEl = tie message
 
 
 const winningArrays = [
